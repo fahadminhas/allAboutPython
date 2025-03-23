28e8ef38953a597aac4107c65ee75067514c68f7
fahadminhas <mfahadm.official@gmail.com>
2025-03-23 19:27:44 +0000

    Input and type casting
    input from user, implicit and explicit type casting

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ str = "String type"
 num = 123
 
 print(str,"left one is string, right one is numebr",num);
+
+
+# input function
+#inpt = input("Enter a number: ")
+#print(inpt);
+
+# type casting
+a=1
+b=2.4
+
+# explit type casting
+print(a+int(b));
+
+# implicit type casting
+print(a+b);
